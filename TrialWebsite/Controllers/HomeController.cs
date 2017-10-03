16647ab0d28a42f63f331759a6bf6f8c16b878f1
godfather2024 <dsura001@ucr.edu>
2017-10-02 22:08:56 -0700

    Initial Check In, contact form

---
+++ TrialWebsite/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TrialWebsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Get in Touch with us";
+            return View();
+        }
+    }
+}
---
+++ TrialWebsite/Models/ContactForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TrialWebsite.Models
+{
+    public class ContactForm
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Message { get; set; }
+        [Required]
+        public int PhoneNumber { get; set; }
+    }
+}

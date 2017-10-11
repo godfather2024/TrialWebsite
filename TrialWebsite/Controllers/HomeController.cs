a8d80edcf96d3dbea4418b303bbf02532ed98cab
godfather2024 <dsura001@ucr.edu>
2017-10-10 20:47:04 -0700

    Added Custom css

--- TrialWebsite/Controllers/HomeController.cs
+++ TrialWebsite/Controllers/HomeController.cs
@@ -14,21 +14,38 @@ namespace TrialWebsite.Controllers
     {
         // GET: Home
         [ActionName("Index")]
-        public ActionResult Index()
-        {
-            return View();
-        }
-
-
-        public async Task<ActionResult> Contact(ContactForm cf)
+        public async Task<ActionResult> Index(ContactForm cf)
         {
             if (ModelState.IsValid)
             {
                 var body = "Email from : {0} , Their contact info email: {1}  Ph no : {2} and Message {3}";
                 var message = new MailMessage();
-                message.To.Add(new MailAddress("shwetaybm@gmail.com"));
+                message.To.Add(new MailAddress("godfather2024@gmail.com"));
                 message.Subject = "Inquires";
                 message.Body = string.Format(body, cf.Name, cf.Email, cf.PhoneNumber, cf.Message);
+                message.IsBodyHtml = true;
+                using (var smtp = new SmtpClient())
+                {
+                    await smtp.SendMailAsync(message);
+                    return RedirectToAction("Index");
+                }
+
+            }
+            ViewBag.Message = "Get in Touch with us";
+            return View(cf);
+            
+        }
+
+
+        public async Task<ActionResult> Contact(ContactForm af)
+        {
+            if (ModelState.IsValid)
+            {
+                var body = "Email from : {0} , Their contact info email: {1}  Ph no : {2} and Message {3}";
+                var message = new MailMessage();
+                message.To.Add(new MailAddress("godfather2024@gmail.com"));
+                message.Subject = "Inquires";
+                message.Body = string.Format(body, af.Name, af.Email, af.PhoneNumber, af.Message);
                 message.IsBodyHtml = true;
                 using (var smtp = new SmtpClient())
                 {
@@ -38,7 +55,7 @@ namespace TrialWebsite.Controllers
 
             }
             ViewBag.Message = "Get in Touch with us";
-            return View(cf);
+            return View(af);
         }
 
         [ActionName("Sent")]
